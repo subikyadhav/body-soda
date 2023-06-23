7e0af21a0eb15d33cfc1e68a9f1e90dd15367a6a
subikyadhav <subikshanofficial@gmail.com>
2023-06-23 09:50:55 +0530

    first commit

--- C program to find number of days in month.py
+++ C program to find number of days in month.py
@@ -1,11 +1,10 @@
 value=int(input( " enter the month : "))
-if('month' == 1):
+if(value == 1):
    print(" 31 day")
-elif('month' == 3,5,7,9,11):
+elif(value == 3 or value == 5,7,9,11):
      print(" 31 days ")
 elif('month' == 4,6,8,10,12):
      print(" 30 days ")
 else:
-    ( ' month' == 2)
     print(" 28 days ")     
             

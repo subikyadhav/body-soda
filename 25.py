8e6b848f96e7c9730a50d1cdc299b87d5239d27c
subikyadhav <subikshanofficial@gmail.com>
2023-07-02 23:04:31 +0530

    Merge branch 'main' of http://github.com/subikyadhav/body-soda

---
+++ 25.py
@@ -0,0 +1,18 @@
+a=int(input("enter the value :"))
+if(a==1):
+    b=int(input("enter the radius of circle :"))
+    c=3.14*b*b
+    print(c)
+
+elif(a == 2):
+    d=int(input("enter the base of the triangle :"))
+    e=int(input("enter the height of the triangle :"))
+    f=d*e/2
+    print(f)
+
+else:   
+    g=int(input("enter the length of rectangle :"))
+    h=int(input("enter the width of rectangle :"))
+    i=g*h
+    print(i)
+
---
+++ 26.py
@@ -0,0 +1,11 @@
+a=int(input("enter the 1st integer :" ))
+b=int(input("enter the 2nd integer :"))
+c=int(input("enter the option :"))
+if(c==1):
+    print(a+b)
+if(c==2):
+    print(a-b)    
+if(c==3):    
+    print(a*b)
+if(c == 4):    
+    print(a/b)

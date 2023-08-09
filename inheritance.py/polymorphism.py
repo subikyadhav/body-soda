589b77c3de1c49caf514513b619d8d6a038b4f3b
subikyadhav <subikshanofficial@gmail.com>
2023-08-09 11:03:03 +0530

    9/8/2023

---
+++ inheritance.py/polymorphism.py
@@ -0,0 +1,24 @@
+# class cinema :
+
+#     def run (self):
+#         print(" i cant do action scenes")
+
+# class hero(cinema):
+#     def run (self):
+#         print("i can do action scenes ")
+
+# class  dancer(cinema):
+#     def run (self):
+#         print()       
+
+class Animal():
+    
+    def eat(self):
+        print("This animal is eating")
+    
+class Rabbit(Animal):
+
+    pass
+
+rabbit = Rabbit()
+rabbit.eat()
